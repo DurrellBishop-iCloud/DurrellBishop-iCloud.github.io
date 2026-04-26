838271dd8577a905a95256e9de9fd981d9ef5eb1
Durrell <durrellbishop1@gmail.com>
2026-04-26 17:40:56 +0100

    Widen axis rails and recenter calendar stage

--- seasalter-tides/app.js
+++ seasalter-tides/app.js
@@ -1,5 +1,5 @@
-import { createTidePredictor } from "./neaps-tide-predictor.js?v=0.8.16";
-import { ENGINE_CONFIG, CONSTITUENTS } from "./tide-engine-data.js?v=0.8.16";
+import { createTidePredictor } from "./neaps-tide-predictor.js?v=0.8.17";
+import { ENGINE_CONFIG, CONSTITUENTS } from "./tide-engine-data.js?v=0.8.17";
 
 const MONTHS = [
   "January",
--- seasalter-tides/tide-engine-data.js
+++ seasalter-tides/tide-engine-data.js
@@ -1,5 +1,5 @@
 export const ENGINE_CONFIG = {
-  appVersion: "v0.8.16",
+  appVersion: "v0.8.17",
   stationName: "Seasalter",
   stationLabel: "Seasalter, Kent",
   referenceStation: "Herne Bay (TICON-4 / CMEMS chart datum station)",
