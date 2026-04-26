fb4d14ed92e98aedc870f0a0209bdc8ac5de3fba
Durrell <durrellbishop1@gmail.com>
2026-04-26 16:27:08 +0100

    Add asset cache busting for tide app

--- seasalter-tides/tide-engine-data.js
+++ seasalter-tides/tide-engine-data.js
@@ -1,5 +1,5 @@
 export const ENGINE_CONFIG = {
-  appVersion: "v0.8.1",
+  appVersion: "v0.8.2",
   stationName: "Seasalter",
   stationLabel: "Seasalter, Kent",
   referenceStation: "Herne Bay (TICON-4 / CMEMS chart datum station)",
